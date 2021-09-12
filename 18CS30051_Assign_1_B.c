a29433a56f087c2b73f68bbb1892e06247b7632c
Debajyoti Dasgupta <debajyotidasgupta6@gmail.com>
2021-09-12 12:36:40 +0530

    Working Part B

---
+++ 18CS30051_Assign_1_B.c
@@ -0,0 +1,656 @@
+#include <linux/module.h>  /* Needed by all modules */
+#include <linux/kernel.h>  /* Needed for KERN_ALERT */
+#include <linux/init.h>    /* Needed for the macros */
+#include <linux/proc_fs.h> /* Needed for the proc filesystem */
+#include <linux/uaccess.h> /* Needed for copy_from_user */
+#include <linux/mutex.h>   /* Needed for mutex */
+#include <linux/types.h>   /* Needed for size_t */
+#include <linux/slab.h>    /* Needed for kmalloc */
+#include <linux/string.h>  /* Needed for strlen */
+#include <linux/sched.h>   /* Needed for current */
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Debajyoti Dasgupta");
+MODULE_DESCRIPTION("LKM for Deque");
+MODULE_VERSION("0.1");
+
+#define PROC_FILENAME "partb_1_18CS30051"
+#define MAX_PROCESS_LIMIT 128
+#define MAX_BUFF_SIZE 256
+
+DEFINE_MUTEX(read_write_mutex);
+DEFINE_MUTEX(open_close_mutex);
+
+static struct proc_ops file_ops;
+static char *buffer = NULL;
+static ssize_t buffer_len = 0;
+static size_t MAX_DEQUE_SIZE = 0;
+
+int iter, err_code, process_index, process_state, deque_size, data;
+
+/**
+ * @brief Structure to store the date of deque
+ * @details This structure is used to store  the  data  which is
+ *          to be stored in the deque in the form of a linked list.
+ * @param max_size Maximum size of the deque
+ * @param front_ptr Pointer to the front of the deque
+ * @param rear_ptr Pointer to the rear of the deque
+ * @param size Current size of the deque
+ */
+
+struct ListNode
+{
+    int data;
+    struct ListNode *next;
+} * left_elem, *new_node, *temp;
+
+struct Deque
+{
+    int max_size;
+    struct ListNode *front_ptr;
+    struct ListNode *rear_ptr;
+    int size;
+};
+
+/**
+ * @brief Descriptors for the state of the process
+ */
+
+enum process_state
+{
+    PROC_FILE_OPEN,
+    PROC_DEQUE_CREATE,
+    PROC_DEQUE_WRITE,
+    PROC_FILE_CLOSE
+};
+
+/**
+ * @brief Structure to store the data of a unit process
+ * @details This structure is used to store the data of a unit process
+ *         in the form of a structure.
+ */
+
+struct unit_process
+{
+    pid_t pid;
+    enum process_state state;
+    struct Deque *proc_deque;
+};
+
+struct many_process
+{
+    struct unit_process *process_queue;
+    size_t number_of_process;
+};
+
+struct many_process *process_list;
+
+/**
+ * Utility Functions
+ * -----------------
+ * create_node()  - Creates a new node
+ * create_deque() - Creates a new deque
+ * push_front()   - Pushes a new node to the front of the deque
+ * push_rear()    - Pushes a new node to the rear of the deque
+ * delete_front() - Deletes the front node of the deque
+ */
+
+// Utility function for Deque
+struct ListNode *create_node(int data);
+struct Deque *create_deque(int max_size);
+struct ListNode *insert_front(struct Deque *deque, int data);
+struct ListNode *insert_rear(struct Deque *deque, int data);
+bool delete_front(struct Deque *deque);
+void delete_deque(struct Deque *deque);
+static int init_proc_deque(size_t process_index);
+static int insert_proc_deque(size_t process_index);
+
+// Utility function for process
+static size_t handle_read(size_t process_index);
+static ssize_t handle_write(size_t process_index);
+static void init_process_list(void);
+static int file_open(struct inode *inode, struct file *file);
+static int file_close(struct inode *inode, struct file *file);
+static ssize_t file_read(struct file *file, char *buf, size_t len, loff_t *offset);
+static ssize_t file_write(struct file *file, const char *buf, size_t len, loff_t *offset);
+
+// Utility function for file operations
+size_t get_list_index(pid_t pid);
+
+/**
+ * Kernel Module Functions
+ * -----------------------
+ * lkm_init() - Initializes the kernel module
+ * lkm_exit() - Exits the kernel module and frees the memory
+ */
+
+static int __init lkm_init(void)
+{
+    struct proc_dir_entry *entry = proc_create(PROC_FILENAME, 0666, NULL, &file_ops);
+    if (!entry)
+        return -ENOENT;
+
+    /* Initialize the function pointers of file_ops */
+    file_ops.proc_open = file_open;
+    file_ops.proc_release = file_close;
+    file_ops.proc_read = file_read;
+    file_ops.proc_write = file_write;
+
+    init_process_list();                                                // Initialize the process list
+    buffer = (char *)kmalloc(MAX_BUFF_SIZE * sizeof(char), GFP_KERNEL); // Allocate memory for the buffer
+
+    printk(KERN_ALERT "Deque LKM Inserted Successfully\n");
+    return 0;
+}
+
+static void __exit lkm_exit(void)
+{
+    remove_proc_entry(PROC_FILENAME, NULL);
+    kfree(process_list);
+    printk(KERN_ALERT "Deque LKM Removed Successfully\n");
+}
+
+module_init(lkm_init);
+module_exit(lkm_exit);
+
+// ------- END OF LOADABLE KERNEL MODULE CODE -------
+
+/**
+ * Function Definitions
+ * --------------------
+ */
+
+/**
+ * @brief Initializes the process list and the Deques
+ * @details This function initializes the process list and the Deques
+ *         for each process.
+ */
+
+void init_process_list(void)
+{
+    process_list = (struct many_process *)kmalloc(sizeof(struct many_process), GFP_KERNEL);
+    process_list->process_queue = (struct unit_process *)kmalloc(sizeof(struct unit_process) * MAX_PROCESS_LIMIT, GFP_KERNEL);
+    process_list->number_of_process = 0;
+
+    for (iter = 0; iter < MAX_PROCESS_LIMIT; iter++)
+    {
+        process_list->process_queue[iter].pid = -1;
+        process_list->process_queue[iter].state = PROC_FILE_CLOSE;
+        process_list->process_queue[iter].proc_deque = NULL;
+    }
+}
+
+/**
+ * @brief Open proc file for interaction
+ * @details This function is called when the proc file is to be opened.
+ *         It checks if the process is already opened or not.
+ * @param inode Inode of the proc file
+ * @param file File structure of the proc file
+ * @return Returns the error code
+ */
+
+int file_open(struct inode *inode, struct file *file)
+{
+    err_code = 0;
+    mutex_lock(&open_close_mutex);
+
+    process_index = get_list_index(current->pid);
+
+    // If the process is already opened or the process list is full and needs to be inserted
+    if (process_index == MAX_PROCESS_LIMIT || process_list->process_queue[process_index].state != PROC_FILE_CLOSE)
+    {
+        err_code = -EINVAL; // Too many files open
+        goto exit_file_open;
+    }
+
+    // Initialize the process
+    process_list->process_queue[process_index].pid = current->pid;
+    process_list->process_queue[process_index].state = PROC_FILE_OPEN;
+    process_list->process_queue[process_index].proc_deque = create_deque(MAX_DEQUE_SIZE);
+    process_list->number_of_process++;
+
+    printk(KERN_ALERT "Process %d opened\n", current->pid);
+
+    // Return the error code
+exit_file_open:
+    mutex_unlock(&open_close_mutex);
+    return err_code;
+}
+
+int file_close(struct inode *inode, struct file *file)
+{
+    err_code = 0;
+    mutex_lock(&open_close_mutex);
+
+    process_index = get_list_index(current->pid);
+
+    // If the process is not opened or file that needs to be closed is not the current file
+    if (process_index == MAX_PROCESS_LIMIT || process_list->process_queue[process_index].pid != current->pid)
+    {
+        err_code = -EINVAL; // File not opened
+        goto exit_file_close;
+    }
+
+    // Delete the deque
+    if (process_list->process_queue[process_index].state >= PROC_DEQUE_CREATE)
+    {
+        delete_deque(process_list->process_queue[process_index].proc_deque);
+        process_list->process_queue[process_index].proc_deque = NULL;
+    }
+
+    // Set the state of the process to closed
+    process_list->process_queue[process_index].state = PROC_FILE_CLOSE;
+    process_list->process_queue[process_index].pid = -1;
+    process_list->number_of_process--;
+
+    printk(KERN_ALERT "Process %d closed\n", current->pid);
+
+    // Return the error code
+exit_file_close:
+    mutex_unlock(&open_close_mutex);
+    return err_code;
+}
+
+ssize_t file_read(struct file *file, char *buf, size_t len, loff_t *offset)
+{
+    err_code = 0;
+    process_index = get_list_index(current->pid);
+
+    // Process not found
+    if (process_index == MAX_PROCESS_LIMIT || process_list->process_queue[process_index].pid != current->pid)
+    {
+        err_code = -EINVAL;
+        goto exit_file_read;
+    }
+
+    // empty buffer
+    if (!buf || !len)
+        return -EINVAL;
+
+    mutex_lock(&read_write_mutex);
+    buffer_len = len < MAX_BUFF_SIZE ? len : MAX_BUFF_SIZE;
+    err_code = handle_read(process_index);
+
+    if (!err_code && copy_to_user(buf, buffer, buffer_len))
+        err_code = -ENOBUFS;
+    if (!err_code)
+        err_code = buffer_len;
+
+    mutex_unlock(&read_write_mutex);
+
+    // Return the error code
+exit_file_read:
+    return err_code;
+}
+
+ssize_t file_write(struct file *file, const char *buf, size_t len, loff_t *offset)
+{
+    err_code = 0;
+    process_index = get_list_index(current->pid);
+
+    // Process not found
+    if (process_index == MAX_PROCESS_LIMIT || process_list->process_queue[process_index].pid != current->pid)
+    {
+        err_code = -EINVAL;
+        goto exit_file_write;
+    }
+
+    // empty buffer
+    if (!buf || !len)
+        return -EINVAL;
+
+    mutex_lock(&read_write_mutex);
+    buffer_len = len < MAX_BUFF_SIZE ? len : MAX_BUFF_SIZE;
+
+    if (copy_from_user(buffer, buf, buffer_len))
+        err_code = -ENOBUFS;
+
+    if (!err_code)
+        err_code = handle_write(process_index);
+    if (!err_code)
+        err_code = buffer_len;
+    mutex_unlock(&read_write_mutex);
+
+    // Return the error code
+exit_file_write:
+    return err_code;
+}
+
+/**
+ * @brief Get the index of the process id in the process list
+ * @details This function returns the index of the process id in the
+ *       process list.
+ * @param pid Process id of the process
+ * @return Returns the index of the process id in the process list
+ *        Returns minimum index which is free in the process list if
+ *        the process id is not found
+ */
+
+size_t get_list_index(pid_t pid)
+{
+    iter = 0;
+
+    // Search for the process id in the process list
+    for (iter = 0; iter < MAX_PROCESS_LIMIT; iter++)
+    {
+        if (process_list->process_queue[iter].pid == pid)
+        {
+            return iter;
+        }
+    }
+
+    // If the process id is not found, return the minimum index which is free
+    for (iter = 0; iter < MAX_PROCESS_LIMIT; iter++)
+    {
+        if (process_list->process_queue[iter].pid == -1 && process_list->process_queue[iter].state == PROC_FILE_CLOSE)
+        {
+            return iter;
+        }
+    }
+
+    // If all the indexes are full, return MAX_PROCESS_LIMIT
+    return MAX_PROCESS_LIMIT;
+}
+
+/**
+ * @brief Handle the read operation
+ * @details This function handles the read operation.
+ * @param process_index Index of the process in the process list
+ * @return Returns the error code
+ * @note This function is called with the read_write_mutex locked
+ */
+
+static size_t handle_read(size_t process_index)
+{
+    process_state = process_list->process_queue[process_index].state;
+    if (process_state < PROC_DEQUE_CREATE)
+    {
+        return -EACCES;
+    }
+
+    left_elem = process_list->process_queue[process_index].proc_deque->front_ptr;
+    if (left_elem != NULL)
+    {
+        data = left_elem->data;
+        if (!delete_front(process_list->process_queue[process_index].proc_deque))
+        {
+            buffer[0] = -1;
+            return -EACCES;
+        }
+        strncpy(buffer, (const char *)&data, sizeof(int));
+        buffer[sizeof(left_elem->data)] = '\0';
+        return 0;
+    }
+    else
+    {
+        buffer[0] = -1;
+        return -EACCES;
+    }
+
+    // Copy the data to the buffer
+    buffer[0] = '\0';
+    return 0;
+}
+
+/**
+ * @brief Handle the write operation
+ * @details This function handles the write operation.
+ * @param process_index Index of the process in the process list
+ * @return Returns the error code
+ * @note This function is called with the read_write_mutex locked
+ */
+
+static ssize_t handle_write(size_t process_index)
+{
+    process_state = process_list->process_queue[process_index].state;
+    err_code = 0;
+
+    if (process_state == PROC_FILE_OPEN)
+    {
+        // Initialize the deque
+        err_code = init_proc_deque(process_index);
+        if (err_code >= 0)
+        {
+            process_list->process_queue[process_index].state = PROC_DEQUE_CREATE;
+        }
+    }
+    else if (process_state >= PROC_DEQUE_CREATE)
+    {
+        // Add the data to the deque
+        err_code = insert_proc_deque(process_index);
+        if (err_code >= 0)
+        {
+            process_list->process_queue[process_index].state = PROC_DEQUE_WRITE;
+        }
+    }
+    else
+    {
+        err_code = -EACCES;
+    }
+
+    return err_code;
+}
+
+/**
+ * @brief Function to create a new node
+ * @details This function is used to create a new node in the deque.
+ * @param data Data to be stored in the node
+ * @return Returns the pointer to the new node
+ * @retval NULL If the node cannot be created
+ * @retval !NULL If the node is created successfully
+ */
+
+struct ListNode *create_node(int data)
+{
+    struct ListNode *new_node = (struct ListNode *)kmalloc(sizeof(struct ListNode), GFP_KERNEL);
+    if (new_node == NULL)
+    {
+        printk(KERN_ALERT "Cannot create node\n");
+        return NULL;
+    }
+    new_node->data = data;
+    new_node->next = NULL;
+    return new_node;
+}
+
+/**
+ * @brief Function to create a new deque
+ * @details This function is used to create a new deque.
+ * @param max_size Maximum size of the deque
+ * @return Returns the pointer to the new deque
+ * @retval NULL If the deque cannot be created
+ * @retval !NULL If the deque is created successfully
+ */
+
+struct Deque *create_deque(int max_size)
+{
+    struct Deque *new_deque = (struct Deque *)kmalloc(sizeof(struct Deque), GFP_KERNEL);
+    if (new_deque == NULL)
+    {
+        printk(KERN_ALERT "Cannot create deque\n");
+        return NULL;
+    }
+    new_deque->max_size = max_size;
+    new_deque->front_ptr = NULL;
+    new_deque->rear_ptr = NULL;
+    new_deque->size = 0;
+    return new_deque;
+}
+
+/**
+ * @brief Function to insert a node at the front of the deque
+ * @details This function is used to insert a node at the front of the deque.
+ * @param deque Pointer to the deque
+ * @param data Data to be stored in the node
+ * @return Returns the pointer to the new node
+ * @retval NULL If the node cannot be inserted
+ * @retval !NULL If the node is inserted successfully
+ */
+
+struct ListNode *insert_front(struct Deque *deque, int data)
+{
+    if (deque->size == deque->max_size)
+    {
+        printk(KERN_ALERT "Deque is full\n");
+        return NULL;
+    }
+
+    new_node = create_node(data);
+    if (new_node == NULL)
+    {
+        printk(KERN_ALERT "Cannot insert node\n");
+        return NULL;
+    }
+    if (deque->size == 0)
+    {
+        deque->front_ptr = new_node;
+        deque->rear_ptr = new_node;
+    }
+    else
+    {
+        new_node->next = deque->front_ptr;
+        deque->front_ptr = new_node;
+    }
+    deque->size++;
+    return new_node;
+}
+
+/**
+ * @brief Function to insert a node at the rear of the deque
+ * @details This function is used to insert a node at the rear of the deque.
+ * @param deque Pointer to the deque
+ * @param data Data to be stored in the node
+ * @return Returns the pointer to the new node
+ * @retval NULL If the node cannot be inserted
+ * @retval !NULL If the node is inserted successfully
+ */
+
+struct ListNode *insert_rear(struct Deque *deque, int data)
+{
+    if (deque->size == deque->max_size)
+    {
+        printk(KERN_ALERT "Deque is full\n");
+        return NULL;
+    }
+
+    new_node = create_node(data);
+    if (new_node == NULL)
+    {
+        printk(KERN_ALERT "Cannot insert node\n");
+        return NULL;
+    }
+    if (deque->size == 0)
+    {
+        deque->front_ptr = new_node;
+        deque->rear_ptr = new_node;
+    }
+    else
+    {
+        deque->rear_ptr->next = new_node;
+        deque->rear_ptr = new_node;
+    }
+    deque->size++;
+    return new_node;
+}
+
+/**
+ * @brief Function to delete a node from the front of the deque
+ * @details This function is used to delete a node from the front of the deque.
+ * @param deque Pointer to the deque
+ * @return boolean Returns true if the node is deleted successfully
+ * @retval true If the node is deleted successfully
+ * @retval false If the node cannot be deleted
+ */
+
+bool delete_front(struct Deque *deque)
+{
+    if (deque->size == 0)
+    {
+        printk(KERN_ALERT "Deque is empty\n");
+        return false;
+    }
+    temp = deque->front_ptr;
+    if (deque->size == 1)
+    {
+        deque->front_ptr = NULL;
+        deque->rear_ptr = NULL;
+    }
+    else
+    {
+        deque->front_ptr = deque->front_ptr->next;
+    }
+    kfree(temp);
+    deque->size--;
+    return true;
+}
+
+/**
+ * @brief Function to delete the deque
+ * @details This function is used to delete the deque.
+ * @param deque Pointer to the deque
+ * @return boolean Returns true if the deque is deleted successfully
+ */
+void delete_deque(struct Deque *deque)
+{
+    while (deque->size > 0)
+    {
+        delete_front(deque);
+    }
+    kfree(deque);
+}
+
+/**
+ * @brief Function to initialize the deque attributes
+ * @details This function is used to initialize the deque attributes.
+ * @param process index of the process
+ * @return error code
+ * @retval 0 If the deque attributes are initialized successfully
+ */
+
+static int init_proc_deque(size_t process_index)
+{
+    if (buffer_len > sizeof(char))
+    {
+        printk(KERN_ALERT "Buffer length is too large\n");
+        return -EINVAL;
+    }
+
+    deque_size = (size_t)buffer[0];
+    if (deque_size < 1 || deque_size > 100)
+    {
+        printk(KERN_ALERT "Deque size is invalid\n");
+        return -EINVAL;
+    }
+
+    return ((process_list->process_queue[process_index].proc_deque = create_deque(deque_size)) != NULL ? 0 : -EINVAL);
+}
+
+/**
+ * @brief Function to insert an integer into the deque
+ * @details This function is used to insert an integer into the deque.
+ * @param process index of the process
+ * @param data integer to be inserted
+ * @return error code
+ * @retval len If the integer is inserted successfully
+ */
+
+static int insert_proc_deque(size_t process_index)
+{
+    if (buffer_len != sizeof(int32_t))
+    {
+        return -EINVAL;
+    }
+
+    data = *(int32_t *)buffer;
+
+    if (data % 2 == 0)
+    {
+        return ((insert_rear(process_list->process_queue[process_index].proc_deque, data) != NULL) ? buffer_len : -EACCES);
+    }
+    else
+    {
+        return ((insert_front(process_list->process_queue[process_index].proc_deque, data) != NULL) ? buffer_len : -EACCES);
+    }
+
+    return 0;
+}
---
+++ temp.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <sys/types.h>
+
+int main()
+{
+    int fd, fd2, pid;
+    char filename[100] = "/proc/partb_1_18CS30051";
+    pid = fork();
+    //fork(); //uncomment this to check for >2 processes
+    if (pid == 0)
+    {
+        printf("%d Opening File\n", getpid());
+        fd = open(filename, O_RDWR);
+        if (fd < 0)
+        {
+            printf("%d Cannot open file...\n", getpid());
+            printf("%d Error= %d\n", getpid(), errno);
+            return 0;
+        }
+        fd2 = open(filename, O_RDWR);
+        //check reopening for file
+        if (fd2 < 0)
+        {
+            printf("%d Cannot open file again...\n", getpid());
+            printf("%d Error= %d\n", getpid(), errno);
+            printf("%d fd2=%d\n", getpid(), fd2);
+        }
+        //7 9 10 12-> write order
+        //9 7 10 12-> expected deque structure
+        int a = 4;
+        u_int8_t b = 4;
+        int x;
+        if ((x = write(fd, &b, sizeof(b))) < 0)
+        {
+            printf("%d error %d\n", getpid(), errno);
+            return 0;
+        }
+        a = 7;
+        x = write(fd, &a, sizeof(int));
+        a = 9;
+        x = write(fd, &a, sizeof(int));
+        //sleep(2); //uncomment this for proper printing
+        a = 10;
+        x = write(fd, &a, sizeof(int));
+        a = 12;
+        x = write(fd, &a, sizeof(int));
+        a = 101;
+        x = write(fd, &a, sizeof(int));
+        printf("%d Writing to file : %d\n", getpid(), errno);
+        a = 99;
+        x = write(fd, &a, sizeof(int));
+        printf("%d Writing to file : %d\n", getpid(), errno);
+
+        read(fd, &a, sizeof(int));
+        printf("%d read %d expected 9\n", getpid(), a); //expected 9
+        read(fd, &a, sizeof(int));
+        printf("%d read %d expected 7\n", getpid(), a); //expected 7
+        //sleep(2); //uncomment this for proper printing
+        read(fd, &a, sizeof(int));
+        printf("%d read %d expected 10\n", getpid(), a); //expected 10
+        read(fd, &a, sizeof(int));
+        printf("%d read %d expected 12\n", getpid(), a); //expected 12
+
+        read(fd, &a, sizeof(int));
+        printf("%d read %d expected ERROR\n", getpid(), a);
+        read(fd, &a, sizeof(int));
+        printf("%d read %d expected ERROR\n", getpid(), a);
+
+        printf("%d Closing file\n", getpid());
+        close(fd);
+
+        //check if we can reopen after close
+        fd = open(filename, O_RDWR);
+        if (fd >= 0)
+        {
+            printf("%d successfully reopened after close\n", getpid());
+            close(fd);
+        }
+        else
+        {
+            printf("%d could not reopen after close\n", getpid());
+        }
+        return 0;
+    }
+    else if (pid > 0)
+    {
+        printf("%d Opening File\n", getpid());
+        fd = open(filename, O_RDWR);
+        if (fd < 0)
+        {
+            printf("%d Cannot open file...\n", getpid());
+            printf("%d Error= %d\n", getpid(), errno);
+            return 0;
+        }
+        int a = 4;
+        u_int8_t b = 4;
+        int x;
+        char str[4] = "abc";
+        if ((x = write(fd, &b, sizeof(b))) < 0)
+        {
+            printf("%d Size error %d\n", getpid(), errno);
+            return 0;
+        }
+        //this should return error on writing 3 bytes
+        if (x = write(fd, str, strlen(str)) < 0)
+        {
+            printf("%d %d write error on string of length 3\n", getpid(), errno);
+        }
+        //11 19 20 17 -> write order
+        //19 11 17 20 -> expected read order
+        a = 11;
+        x = write(fd, &a, sizeof(int));
+        a = 19;
+        x = write(fd, &a, sizeof(int));
+        //sleep(2); //uncomment this for proper printing
+        a = 20;
+        x = write(fd, &a, sizeof(int));
+
+        read(fd, &a, sizeof(int));
+        printf("%d read %d expected 19\n", getpid(), a);
+        read(fd, &a, sizeof(int));
+        printf("%d read %d expected 11\n", getpid(), a);
+
+        a = 17;
+        x = write(fd, &a, sizeof(int));
+
+        //sleep(2); //uncomment this for proper printing
+        read(fd, &a, sizeof(int));
+        printf("%d read %d expected 17\n", getpid(), a);
+        read(fd, &a, sizeof(int));
+        printf("%d read %d expected 20\n", getpid(), a);
+
+        printf("%d Closing file\n", getpid());
+        close(fd);
+
+        return 0;
+    }
+    return 0;
+}
